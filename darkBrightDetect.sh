75f03a7ab13b6b1670031ad3d4814cc65a8e8e75
christopherwmartin <cmarti14@artic.edu>
2016-07-26 11:00:28 -0500

    revised variables

--- darkBrightDetect.sh
+++ darkBrightDetect.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
 
 command -v convert >/dev/null 2>&1 || { echo "This script requires 'ImageMagick'. Aborting." >&2; exit 1; }
-
 command -v bc >/dev/null 2>&1 || { echo "This script requires 'bc'. Aborting." >&2; exit 1; }
 
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-definedThreshhold=0.8
+definedThreshold=0.8
 for file in *.jpg; do
 
     read width height < <(identify -format '%w %h' $file)
     totalPixels=$((width * height))
-    acceptableThreshhold=$(echo "scale=0; ($definedThreshhold * $totalPixels)/1" | bc)
+    acceptableThreshold=$(echo "scale=0; ($definedThreshold * $totalPixels)/1" | bc)
 
     read white black < <(convert $file -format "%[fx:mean*w*h] %[fx:(1-mean)*w*h]" info:)
 
     black=$(echo "scale=0; $black/1" | bc)
     white=$(echo "scale=0; $white/1" | bc)
 
-	  if (("$black" > "$acceptableThreshhold")) ;
+	  if (("$black" > "$acceptableThreshold")) ;
 	then
 	    echo "${bold}removing $file - too dark${normal}"
       #rm $file
 	else
-		if (("$white" > "$acceptableThreshhold")) ;
+		if (("$white" > "$acceptableThreshold")) ;
 		then
 		    echo "${bold}removing $file - too bright${normal}"
         #rm $file
 		fi
 	fi
-  echo "$file - black: $black, white: $white, threshhold: $acceptableThreshhold"
+  echo "$file - black: $black, white: $white, threshhold: $acceptableThreshold"
 done
